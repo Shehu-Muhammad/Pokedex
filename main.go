06e319b5a90bd77bd02ee52af8ba17b71185330b
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-04-08 13:51:22 -0400

    created repl Pokdex to return first word from user input

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin) // Create a scanner for standard input
+	for {
+		fmt.Print("Pokedex > ")
+		scanned := scanner.Scan() // Wait for input
+		if !scanned {             // Break if there's an error or EOF
+			break
+		}
+		text := scanner.Text() // Get the input as a string
+		cleanedText := cleanInput((text))
+		fmt.Println("Your command was:", cleanedText[0]) // Print back the input
+	}
 }
 
 func cleanInput(text string) []string {
